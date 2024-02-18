a5207f23e557611d96259694c17644cda26422e3
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-02-17 23:02:12 -0700

    added function to redirect to the gamePlay

--- frontend/src/components/MainCard.jsx
+++ frontend/src/components/MainCard.jsx
@@ -1,11 +1,23 @@
 import heartSnail from '../assets/heart-snail.jpg';
+import { useNavigate } from 'react-router-dom';
 
 export default function MainCard() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('./GamePlay', { replace: true });
+  };
+
   return (
     <div className='mainCard'>
       <div className='game'>
         <h2 className='gameHeader'>Find the Heart</h2>
-        <img className='heartSnail' src={heartSnail} />
+        <img
+          className='heartSnail'
+          src={heartSnail}
+          alt='Heart Snail'
+          onClick={handleClick}
+        />
       </div>
     </div>
   );
