693317488386529d682667a3cac257c73562b476
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-02-20 13:27:21 -0700

    removed the serverside to restart

--- backend/server.js
+++ backend/server.js
@@ -10,32 +10,6 @@ app.use(
   })
 );
 
-let startTime;
-let timerInterval;
-let heartClicked = false;
-
-app.get('/startTimer', (req, res) => {
-  startTime = Date.now();
-  timerInterval = setInterval(() => {
-    const timeElapsed = Date.now() - startTime;
-    res.json({ timeElapsed: timeElapsed, heartClicked });
-  }, 1000);
-});
-
-let elapsedTimeValue = 0;
-
-app.get('/elapsedTime', (req, res) => {
-  res.json({ elapsedTime: elapsedTimeValue });
-});
-
-app.get('/heartClick', (req, res) => {
-  if (!heartClicked) {
-    heartClicked = true;
-    clearInterval(timerInterval);
-  }
-  res.sendStatus(200);
-});
-
 app.listen(PORT, () => {
   console.log(`Backend server is running on http://localhost:${PORT}`);
 });
