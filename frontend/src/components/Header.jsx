aacfc33214203e7b663ab98fa684f87381715549
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-02-23 19:55:32 -0700

    adding highscore modal

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import dropdownIcon from '../assets/dropdown.svg';
+import HighScoreModal from './HighScoreModal';
 
 export default function Header() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <header className='header'>
-      <div className='logo'>
-        <img src='./src/assets/wheresWaldoLogo.png' className='logoImg' />
-      </div>
-      <div className='high-scores'>
-        <img src={dropdownIcon} alt='Dropdown' className='dropdown' />
-        <h4>High Scores</h4>
-      </div>
-    </header>
+    <>
+      <header className='header'>
+        <div className='logo'>
+          <img src='./src/assets/wheresWaldoLogo.png' className='logoImg' />
+        </div>
+        <div className='high-scores' onClick={handleClick}>
+          <img src={dropdownIcon} alt='Dropdown' className='dropdown' />
+          <h4>High Scores</h4>
+        </div>
+      </header>
+      <div>{showModal && <HighScoreModal onClick={handleCloseModal} />}</div>
+    </>
   );
 }
---
+++ frontend/src/components/HighScoreModal.jsx
@@ -0,0 +1,10 @@
+export default function HighScoreModal(onClick) {
+  return (
+    <>
+      <div>hello world</div>
+      <button className='btn' onClick={onClick}>
+        Close
+      </button>
+    </>
+  );
+}
